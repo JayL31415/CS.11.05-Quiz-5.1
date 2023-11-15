e04259961c278ac9f1743b5f96cc34b5fb51dcef
J <jaylauhc@gmail.com>
2023-11-15 08:26:55 +0800

    Removed redundant comments.

--- src/Main.java
+++ src/Main.java
@@ -8,10 +8,17 @@ public class Main {
      * zip(myArray1, myArray2) → {1,3,5,7,9,2,4,6,8,10,12,14,16}
      */
     public static int[] combine(int[] array1, int[] array2) {
-
-        return null;
-
+        int[] newarray = new int[array1.length + array2.length];
+        for (int i = 0; i < newarray.length; i++) {
+            if (i < array1.length) {
+                newarray[i] = array1[i];
+            } else {
+                newarray[i] = array2[i - array1.length];
+            }
+        }
+        return newarray;
     }
+
 
     /**
      * Given two arrays of integers of equal length, write a method called zip that zips together all elements in each array into one larger array.
@@ -21,10 +28,21 @@ public class Main {
      * zip(myArray1, myArray2) → {1,2,3,4,5,6,7,8,9,10}
      */
     public static int[] zip(int[] array1, int[] array2) {
+        int[] newarray = new int[array1.length + array2.length];
+        int counter = 0;
 
-        return null;
+        for (int i = 0; i < array1.length; i++) {
+            newarray[counter] = array1[i];
+            counter++;
+            newarray[counter] = array2[i];
+            counter++;
+        }
 
+        return newarray;
     }
+
+
+
 
     /**
      * Given two arrays of integers of equal length, write a method called product that multiplies each element in the first array by the element at the corresponding index in the second array.
@@ -34,10 +52,15 @@ public class Main {
      * product(myArray1, myArray2) → {2,12,30,56,90}
      */
     public static int[] product(int[] array1, int[] array2) {
-
-        return null;
-
+        int[] newarray = new int[array1.length];
+        for (int i = 0; i < newarray.length; i++) {
+            newarray[i] = array1[i] * array2[i];
+        }
+        return newarray;
     }
+
+
+
 
     /**
      * Given an array of Strings, write a method called capitalCount that calculates the number of capital letters in each String and stores them into an array.
@@ -56,15 +79,25 @@ public class Main {
      * capitalCount(words) → {1, 2, 2, 0}
      */
     public static int[] capitalCount(String[] words) {
+            int[] capitalCounts = new int[words.length];
+            for (int i = 0; i < words.length; i++) {
+                capitalCounts[i] = countCapitalLetters(words[i]);
+            }
+            return capitalCounts;
+        }
 
-        return null;
-
-    }
 
     public static int countCapitalLetters(String word) {
-
-        return 0;
-
+        int count = 0;
+        for (int i = 0; i < word.length(); i++) {
+            if (Character.isAlphabetic(word.charAt(i)) && word.charAt(i)>=65 && word.charAt(i)<=90) {
+                count++;
+            }
+        }
+        return count;
     }
+
+
+
 
 }
